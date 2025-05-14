0d927190e07c7ce67d8099ef1db08f3ab59d7dfc
Nicolas Deleasa <nicktakesmail@gmail.com>
2025-05-14 13:12:58 -0400

    Initial Commit

---
+++ Pocketpack/PocketpackApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketpackApp.swift
+//  Pocketpack
+//
+//  Created by Nicolas Deleasa on 5/14/25.
+//
+
+import SwiftUI
+
+@main
+struct PocketpackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
